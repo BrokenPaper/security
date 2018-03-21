67f0f39057357a635e81786b5df869cc27b1f535
BrokenPaper <30953850+BrokenPaper@users.noreply.github.com>
2018-03-21 20:05:16 +0800

    稍微修改了下

--- auto_flag.py
+++ auto_flag.py
@@ -5,14 +5,18 @@ import re
 
 url = 'http://192.168.1.100/lms/portal/sp/hz_flag.php'
 
-
 # 修改为自己登陆后获取到的Cookie
+header = {
+    'cookie': 'SSCSum=39; zlms-sid=olm0tcq4ejtmdrka17vs1rdo85; webcs_test_cookie=lms_cookie_checker; lms_login_name=HZ8'
+}
 
 
-def commit_flag(target_host, target_flag, header):
-    res = requests.post(url=url, data={'melee_ip': target_host, 'melee_flag': target_flag}, headers=header)
-    return True if re.search(u'成功', res.content) else False
-
+def commit_flag(target_host, target_flag):
+    try:
+        res = requests.post(timeout=3, url=url, data={'melee_ip': target_host, 'melee_flag': target_flag}, headers=header)
+        print '[√] 已成功提交Flag值' if re.search(u'成功', res.content) else '[X] 提交Flag值失败'
+    except:
+        print '[X] 提交过程中发生了错误'
 
 if __name__ == '__main__':
     parser = OptionParser()
@@ -21,7 +25,5 @@ if __name__ == '__main__':
     parser.add_option('-f', dest='flag', type='string')
 
     (options, args) = parser.parse_args()
-    header = {
-        'cookie': 'SSCSum=39; zlms-sid=olm0tcq4ejtmdrka17vs1rdo85; webcs_test_cookie=lms_cookie_checker; lms_login_name=HZ8'
-    }
-    commit_flag(options.target, options.flag, header)
+
+    commit_flag(options.target, options.flag)
