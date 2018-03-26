791a94570540c64d5c92227d5aa12c7f273e2079
BrokenPaper <30953850+BrokenPaper@users.noreply.github.com>
2018-03-26 10:57:18 +0800

    修复了一个小问题

--- auto_ssh.sh
+++ auto_ssh.sh
@@ -4,7 +4,7 @@ for i in `seq 2 5`
 do
 ip="192.168.100.$i"
 echo "[SSH] Attack $ip..."
-a=`nc -z $ip 22 -v -n|grep open|echo|wc -l`
+a=`nc -z $ip 22 -v -n -w 1 2>&1|grep open|wc -l`
 if [ "$a" -eq "1" ]
 then
 python auto_ssh.py -t $ip
